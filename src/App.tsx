3b97f4804b2d529d498bd043240d344831df9a31
Alexander9673 <alexander.m9673@gmail.com>
2022-06-21 12:36:28 +0800

    Add routing. Todo: make backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
+import {
+  Routes,
+  Route
+} from 'react-router-dom'
+
 // styles
 import './css/index.sass'
 
@@ -6,9 +11,24 @@ import LoginPage from './pages/LoginPage'
 
 const App = () => {
   return (
-    <>
-      <LoginPage />
-    </>
+    <Routes>
+      <Route
+        path='/login'
+        element={<LoginPage />}
+      />
+      
+      { /* default route for 404 TODO: Move to different file */ }
+      <Route
+        path='*'
+        element={
+          (
+            <div>
+              <h1>404 Not found</h1>
+            </div>
+          )
+        }
+      />
+    </Routes>
   )
 }
 
